7706565a13ea5a419c069a562c21b62c47088807
Cleber Batista][ <cleberbatista99@hotmail.com>
2024-11-22 14:07:46 -0300

    Consolidando estudos

---
+++ appteste1.py
@@ -0,0 +1,13 @@
+print("Loja doce sabor")
+
+print("1 pedir um café ")
+print("2 desejo ler um livro")
+print("1 sair da loja ")
+print("1 comprar uma paçoca salgada ")
+
+opcoes_escolhidas = input("Escolha uma das opções: ")
+
+print(f'Vc escolheu a opção de numero {opcoes_escolhidas}')
+
+
+
